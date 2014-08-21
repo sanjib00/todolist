ecc36fc8ee5f5b2039af024cea1f86c5d2a1314f
sanjib00 <sanjib_1@hotmail.com>
2014-08-21 12:05:53 +0600

    version updating process going on

---
+++ app/.env.local.php
@@ -0,0 +1,13 @@
+
+<?php
+
+return [
+    // Database vars
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'root',
+    'DB_PASS' => 'laravel',
+    'DB_NAME' => 'todo',
+
+    // Any other vars you want
+    // ...
+];
